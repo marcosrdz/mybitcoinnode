82620aef9977d207febeb54979c2b629b9265412
Marcos Rodriguez Vélez <rodrigum@us.ibm.com>
2018-07-09 23:22:47 -0400

    Bitseed logo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Server from './Server'
 import Wallet from './Wallet'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return(
       <Router> 
         <React.Fragment>       
-        <Navbar collapseOnSelect inverse>
-            <Navbar.Header>
-                <LinkContainer to="/">        
-                    <Navbar.Brand>
-                        Bitseed
-                    </Navbar.Brand>
-                </LinkContainer>        
-            <Navbar.Toggle />
-            </Navbar.Header>
-            <Nav>
-                <LinkContainer to="/server">
-                    <NavItem eventKey={1}>
-                    Server
-                    </NavItem>
-                </LinkContainer>
-                <LinkContainer to="/wallet">        
-                    <NavItem eventKey={2}>
-                    Wallet
-                    </NavItem>
-                </LinkContainer>
-            </Nav>
-        </Navbar>
+        <Header />
         <Switch>
         <Route exact path='/server' component={Server} />
         <Route exact path='/wallet' component={Wallet} />
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+class Header extends Component {
+  render() {
+    return(
+        <Navbar collapseOnSelect>
+            <Navbar.Header>
+                <LinkContainer to="/server">        
+                    <Navbar.Brand>
+                    <img 
+                        src={require('./images/bitseed.png')} 
+                    />
+                    </Navbar.Brand>
+                </LinkContainer>        
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Nav>
+                <LinkContainer to="/server">
+                    <NavItem eventKey={1}>
+                    Server
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/wallet">        
+                    <NavItem eventKey={2}>
+                    Wallet
+                    </NavItem>
+                </LinkContainer>
+            </Nav>
+        </Navbar>
+    )
+  }
+}
+
+export default Header
