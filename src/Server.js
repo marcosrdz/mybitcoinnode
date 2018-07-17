dedadd08bc0fb43a68fe99f8ac4c8f0b0cfe01d3
Marcos Rodriguez Vélez <rodrigum@us.ibm.com>
2018-07-17 00:16:55 -0400

    cleaned up code

--- src/Server.js
+++ src/Server.js
@@ -15,108 +15,46 @@ export default class Server extends Component {
 
   constructor(props) {
     super()
-    this.state = {bitcoinBlockchainInformation: {}}
+    this.state = {
+      bitcoinBlockchainInformation: { 
+        blocks: '', 
+        pruned: false, 
+        headers: ''
+      }
+    }
     this.getBlockchainInformation()
   }
 
+  renderRowWithColumn(title, description) {
+    return(
+      <Row className="show-grid">
+        <Col xs={1} md={4}></Col>
+        <Col xs={4} md={4}>
+        {title}
+        </Col>
+        <Col xs={4} md={4}>
+        {description}
+        </Col>
+        <Col xs={1} md={4}></Col>
+      </Row>
+    )
+  }
 
   render() {
     return (
         <div >
           <Grid>
-  <Row className="show-grid">
-    <Col xs={12} md={8}>
-      Bitcoin Core Version
-    </Col>
-    <Col xs={6} md={4}>
-      Version
-    </Col>
-  </Row>
-
-  <Row className="show-grid">
-    <Col xs={6} md={4}>
-      Node Type
-    </Col>
-    <Col xs={12} md={4}>
-      { this.state.bitcoinBlockchainInformation.pruned === false ? 'Full Node' : 'Pruned'}
-    </Col>
-  </Row>
-
-  <Row className="show-grid">
-    <Col xs={6} xsOffset={6}>
-      Device At Block
-    </Col>
-    <Col xs={6} md={4}>
-    { this.state.bitcoinBlockchainInformation.blocks}
-    </Col>
-  </Row>
-
-  <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-      Network Block:
-    </Col>
-    <Col md={6} mdPull={6}>
-    { this.state.bitcoinBlockchainInformation.headers}
-    </Col>
-  </Row>
-
-  <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-      Peer Connections:
-    </Col>
-    <Col md={6} mdPull={6}>
-      Peer Connection 
-    </Col>
-  </Row>
-
-    <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-      Tx in Mempool:
-    </Col>
-    <Col md={6} mdPull={6}>
-    Mempool
-    </Col>
-  </Row>
-
-    <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-      Minimum Relay Fee:
-    </Col>
-    <Col md={6} mdPull={6}>
-    Fee
-    </Col>
-  </Row>
-
-
-    <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-    Public IP Address:
-    </Col>
-    <Col md={6} mdPull={6}>
-    Public IP
-    </Col>
-  </Row>
-
-
-    <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-    Device ID:
-    </Col>
-    <Col md={6} mdPull={6}>
-    ID
-    </Col>
-  </Row>
-
-  <Row className="show-grid">
-    <Col md={6} mdPush={6}>
-    Device Version:
-    </Col>
-    <Col md={6} mdPull={6}>
-    Version
-    </Col>
-  </Row>
-
-</Grid>
+            {this.renderRowWithColumn('Bitcoin Core Version','Version')}
+            {this.renderRowWithColumn('Node Type', this.state.bitcoinBlockchainInformation.pruned === false ? 'Full Node' : 'Pruned')}
+            {this.renderRowWithColumn('Device At Block', this.state.bitcoinBlockchainInformation.blocks)}
+            {this.renderRowWithColumn('Network Block', this.state.bitcoinBlockchainInformation.headers)}
+            {this.renderRowWithColumn('Peer Connection', 'No Data')}
+            {this.renderRowWithColumn('Tx in Mempool', 'No Data')}
+            {this.renderRowWithColumn('Minimym Relay Fee', 'No Data')}
+            {this.renderRowWithColumn('Public IP Address', 'No Data')}
+            {this.renderRowWithColumn('Device ID', 'No Data')}
+            {this.renderRowWithColumn('Device Version', 'No Data')}
+        </Grid>
        </div>
     )
   }
