7725c8046ae020ffe5dee97004c616b20a24a256
Marcos Rodriguez Vélez <rodrigum@us.ibm.com>
2018-07-29 19:12:47 -0400

    moved AddressPanel to Device

--- src/Bitcoin.js
+++ src/Bitcoin.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import BitcoinPanel from './Panels/BitcoinPanel'
-import AddressesPanel from './Panels/AddressesPanel'
 
 export default class Bitcoin extends Component {
   render() {
     return (
       <div>
         <BitcoinPanel />
-        <AddressesPanel />
       </div>
     )
   }
--- src/Device.js
+++ src/Device.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import AddressesPanel from './Panels/AddressesPanel'
 
 export default class Device extends Component {
   render() {
     return (
       <div>
-        Coming soon...
+        <AddressesPanel />
       </div>
     )
   }
