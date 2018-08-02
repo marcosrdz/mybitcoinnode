a71dfece113250c71be4343c72b0edf66d471b5d
Marcos Rodriguez Vélez <marcospr@pm.me>
2018-08-01 23:50:58 -0400

    Small helper text
    Signed-off-by: Marcos Rodriguez Vélez <marcospr@pm.me>

--- src/Panels/BitseedSettingsPanel.js
+++ src/Panels/BitseedSettingsPanel.js
@@ -1,7 +1,7 @@
 
 
 import React, { Component } from 'react'
-import { Panel, Button, ControlLabel, FormGroup, FormControl, Form, Col } from 'react-bootstrap'
+import { Panel, Button, ControlLabel, FormGroup, FormControl, Form, Col, HelpBlock } from 'react-bootstrap'
 import APIClient from '../APIClient'
 import Grid from 'react-css-grid'
 
@@ -71,6 +71,7 @@ export default class BitseedSettingsPanel extends Component {
                     </Col>
                     <Col sm={9}>
                       <FormControl type="text" placeholder="localhost" value={this.state.data.webUIBitcoinRPCHost} onChange={this.handleChange} disabled={this.state.isLoading} />
+                      <HelpBlock>If your Bitcoin Server is not hosted in the same machine as this web UI, you may use tools such as <a href="https://www.npmjs.com/package/corsproxy"><strong>corsproxy</strong></a></HelpBlock>
                     </Col>
                   </FormGroup>
 
